34209544c5477693ef7be6028692ab667478092e
Alexei Penkin <iva13293376@yandex.ru>
2022-09-19 14:54:26 +0500

    level-3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -275,8 +275,6 @@ export function App() {
     localStorage.removeItem('token');
     setLoggedIn(false);
     history.push('/');
-    localStorage.removeItem('searchText');
-    localStorage.removeItem('shortFilter');
     setCurrentUser({});
   }
 
