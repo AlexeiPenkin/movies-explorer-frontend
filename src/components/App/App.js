85fc94976e292daa1a85c56fda300e926b550c84
Alexei Penkin <iva13293376@yandex.ru>
2022-10-14 13:41:58 +0500

    level-3

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -185,6 +185,7 @@ export function App() {
     if (localData && value) {
       setValue(value)
       onSearch(localStorageValue)
+      durationSwitch(localChecked)
     }
   }, [localData, filter]);
 
